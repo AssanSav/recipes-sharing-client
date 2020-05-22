f6cd7973973dea14dc02fcd67e01c1575203e4a9
AssanSav <assane.savadogo81@gmail.com>
2020-05-22 02:16:26 -0400

    Routes component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styleSheet/App.css';
 import UserSessionsContainer from "./containers/UserSessionsContainer"
+import Routes from './components/Routes';
 
 function App() {
   return (
     <div className="App">
       <UserSessionsContainer />
+      <Routes />
     </div>
   );
 }
---
+++ src/components/Routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./Login"
+import Signup from "./Signup"
+import NavBar from "./NavBar"
+
+
+const Routes = () => {
+
+    return (
+        <Router>
+            <NavBar />
+            <div>
+                <Switch>
+                    <Route exact path="/login" render={(routerProps) =>
+                        <Login history={routerProps.history}
+                        />}>
+                    </Route>
+                    <Route exact path="/signup">
+                        <Signup />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+
+    )
+}
+
+export default Routes
