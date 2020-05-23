b3eaa66ae17dfe2c610141400536bc0604d9dd81
AssanSav <assane.savadogo81@gmail.com>
2020-05-23 04:38:55 -0400

    add ingredient action creator

---
+++ src/actions/addIngredient.js
@@ -0,0 +1,22 @@
+import { FETCH_TO_ADD_INGREDIENT } from "./types"
+
+
+
+export function addIngredient(formData) {
+
+    return (dispatch) => {
+        return fetch(`http://localhost:3001/api/recipes/${formData.recipe_id}/recipe_ingredients`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({ ingredient: formData })
+        })
+            .then(resp => resp.json())
+            .then(({ingredient}) => {
+                dispatch({ type: FETCH_TO_ADD_INGREDIENT, payload: ingredient })
+            })
+    }
+}
