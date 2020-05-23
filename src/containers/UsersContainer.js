76ead10908ed240b02f16a171b3085fc9e6e43cf
AssanSav <assane.savadogo81@gmail.com>
2020-05-23 02:11:59 -0400

    User container component refact

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import { sessionStatus } from "../actions/sessionStatus"
+
+class UserSessionStatus extends Component {
+
+    componentDidMount() {
+        this.props.sessionStatus()
+    }
+
+    render() {
+        const {isLoggedIn, user} = this.props
+        if (isLoggedIn) {   
+            return (
+                <div>
+                </div>
+            )
+        }
+        else {
+            return <div></div>
+        }
+    }
+}
+
+const mapStateToProps = ({ usersReducer }) => {
+    const {isLoggedIn, user} = usersReducer
+    return {
+        isLoggedIn: isLoggedIn,
+        user: user
+    }
+}
+
+export default connect(mapStateToProps, { sessionStatus })(UserSessionStatus)
