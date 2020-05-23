f1d77b23b150823022a2cc1e61f4859398297650
AssanSav <assane.savadogo81@gmail.com>
2020-05-23 02:08:26 -0400

    Categories action creator

---
+++ src/actions/fetchCategories.js
@@ -0,0 +1,12 @@
+import { FETCH_CATEGORIES } from "./types"
+
+
+export function fetchCategories() {
+    return (dispatch) => {
+        return fetch("http://localhost:3001/api/categories")
+            .then(resp => resp.json())
+            .then(({ categories }) => {
+                dispatch({ type: FETCH_CATEGORIES, payload: categories })
+            })
+    }
+}
