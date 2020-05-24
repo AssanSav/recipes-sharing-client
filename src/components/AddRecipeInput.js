42ef976fdc12d57256de8590f7c9d2947666b403
AssanSav <assane.savadogo81@gmail.com>
2020-05-24 14:19:44 -0400

    Logics and conditionnals added to share AaddRecipeInput component for edit also

--- src/components/AddRecipeInput.js
+++ src/components/AddRecipeInput.js
@@ -2,22 +2,26 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import { addRecipe } from "../actions/addRecipe"
 import { fetchCategories } from "../actions/fetchCategories"
+import { editRecipe } from "../actions/editRecipe"
 import { withRouter } from 'react-router-dom';
 
 
 class AddRecipeInput extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+
+        const {recipe} = props
         this.state = {
-            name: "",
-            directions: "",
-            image: "",
-            category_id: ""
+            id: recipe ? recipe.id : null,
+            name: props.recipe ? recipe.name : "",
+            directions: props.recipe ? recipe.directions :"",
+            image: recipe ? recipe.image :"",
+            category_id: recipe ? recipe.category_id :""
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-
     }
+
 
     componentDidMount() {
         this.props.fetchCategories()
@@ -33,13 +37,19 @@ class AddRecipeInput extends Component {
 
     handleSubmit(e) {
         e.preventDefault()
-        this.props.addRecipe(this.state, { ...this.props })
-        this.setState({
+        if (this.props.match.params.recipeId) {
+            this.props.editRecipe(this.state)
+        }
+        else {
+            this.props.addRecipe(this.state, { ...this.props })
+            this.setState({
             name: "",
             directions: "",
             image: "",
             category_id: ""
         })
+        }
+        
     }
 
     render() {
@@ -69,31 +79,43 @@ class AddRecipeInput extends Component {
                         />
                     </p>
                     <p>
-                        <input
+                        <textarea className="direction" rows="7" cols="50"
                             placeholder="Directions"
                             type="text"
                             name="directions"
                             value={directions}
                             onChange={this.handleChange}
-                        />
+                        >
+                        </textarea>
                     </p>
-                    <input type="submit" value="Create" ></input>
+                    {this.props.match.params.recipeId ? <input type="submit" value="Edit" ></input>: <input type="submit" value="Create" ></input>}
                 </form>
             </div>
         )
     }
 }
 
-const mapStateToProps = ({ categoriesReducer}) => {
-    return {
-        categories: categoriesReducer.categories
+const mapStateToProps = ({ categoriesReducer, recipesReducer }, ownProps) => {
+    if (ownProps.match.params.recipeId) {
+        return {
+            recipe: recipesReducer.recipes.find(r => r.id == ownProps.match.params.recipeId) || recipesReducer.recipe,
+            categories: categoriesReducer.categories,
+        }
     }
+    else {
+        return {
+            categories: categoriesReducer.categories,
+            recipe: null
+        } 
+    }
+   
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
-        addRecipe: recipe => dispatch(addRecipe(recipe, ownProps)),
-        fetchCategories: () => dispatch(fetchCategories())
+        addRecipe: (formData) => dispatch(addRecipe(formData, ownProps)),
+        fetchCategories: () => dispatch(fetchCategories()),
+        editRecipe: (formData) => dispatch(editRecipe(formData, ownProps)),
     }
 }
 
