d6578660d6acec8629e3a1f496c069b1a2c92bee
AssanSav <assane.savadogo81@gmail.com>
2020-05-25 23:08:21 -0400

    Remove recipe action creator

---
+++ src/actions/removeRecipe.js
@@ -0,0 +1,20 @@
+import { FETCH_TO_DELETE_RECIPE } from "./types"
+
+
+export function removeRecipe(recipe) {
+    return (dispatch) => {
+        return fetch(`http://localhost:3001/api/recipes/${recipe.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(recipe),
+            credentials: "include"
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                dispatch({ type: FETCH_TO_DELETE_RECIPE, payload: data.recipe }
+                )
+        } )
+    }
+}
