4abda095de934e96229fc4b16127e0c93dae54ce
AssanSav <assane.savadogo81@gmail.com>
2020-05-28 00:46:31 -0400

    Sending props to the NavBar component

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
 import { sessionStatus } from "../actions/sessionStatus"
+import NavBar from "../components/NavBar"
 
 class UserSessionStatus extends Component {
 
@@ -8,13 +9,13 @@ class UserSessionStatus extends Component {
         this.props.sessionStatus()
     }
 
-    render() {
+    render() { 
         return (
             <div>
+                <NavBar isLoggedIn={this.props.isLoggedIn} user={this.props.user} />
             </div>
         )
     }
-
 }
 
 const mapStateToProps = ({ usersReducer }) => {
