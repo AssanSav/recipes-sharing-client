24d7367f69d4737c56050ca6b120631bc081998c
AssanSav <assane.savadogo81@gmail.com>
2020-07-30 15:20:59 -0400

    Errors notification added in case of failed login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,70 +1,83 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux"
-import { loginUser } from "../actions/loginUser"
-
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { loginUser } from "../actions/loginUser";
+import {withRouter} from "react-router-dom"
 
 class Login extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            email: '',
-            password: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.loginUser(this.state);
+    this.setState({
+      email: "",
+      password: "",
+    });
+  }
 
-
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.loginUser(this.state)
-        this.setState({
-            email: '',
-            password: ''
-        })
-        this.props.history.push("/recipes")
-    }
-
-
-    render() {
-        const { email, password } = this.state
-        return (
-            <div>
-                <h1>Log In</h1>
-
-                <form onSubmit={this.handleSubmit}>
-                    <p>
-                        <input
-                            placeholder="email"
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={this.handleChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            placeholder="password"
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={this.handleChange}
-                        />
-                    </p>
-                    <input type="submit" value="Log In" />
-                </form>
-            </div>
-        )
-    }
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <div style={{ color: "red" }}>{this.props.emailEr}</div>
+            <input
+              placeholder="email"
+              type="text"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <div style={{ color: "red" }}>{this.props.passwordEr}</div>
+            <input
+              placeholder="password"
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <input type="submit" value="Log In" />
+        </form>
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = ({ usersReducer }) => {
+  return {
+    emailEr: usersReducer.emailEr,
+    passwordEr: usersReducer.passwordEr,
+  };
+};
 
-export default connect(null, { loginUser })(Login)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    loginUser: (formDAta) => dispatch(loginUser(formDAta, ownProps)),
+  };
+};
+
+const customConnectComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
+
+export default withRouter(customConnectComponent);
