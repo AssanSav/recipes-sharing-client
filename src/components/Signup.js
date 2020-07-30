79b1d51d056e5b9ac6d0f6390798af75596701a0
AssanSav <assane.savadogo81@gmail.com>
2020-07-30 15:19:52 -0400

    Errors notification added in case of failed signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,96 +1,113 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux"
-import { signupUser } from "../actions/signupUser"
-
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signupUser } from "../actions/signupUser";
 
 class Signup extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: '',
-            email: '',
-            password: '',
-            password_confirmation: '',
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.signupUser(this.state).then(() => {
+      if (this.props.status === 409) {
+        this.props.history.push("/signup");
+      } else {
+        this.props.history.push("/")
+      }
+    });
+    this.setState({
+      username: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    });
+  }
 
+  render() {
+    const { email, username, password, password_confirmation } = this.state;
+    return (
+      <div>
+        <h1>Sign Up</h1>
 
-    handleSubmit(e) {
-        e.preventDefault()
-        this.props.signupUser(this.state)
-            .then(() => {
-                this.props.history.push("/recipes")
-        })
-        this.setState({
-            username: '',
-            email: '',
-            password: '',
-            password_confirmation: ""
-        })
-    }
-
-
-    render() {
-        const { email, username, password, password_confirmation } = this.state
-        return (
-            <div>
-                <h1>Sign Up</h1>
-
-                <form onSubmit={this.handleSubmit}>
-                    <p>
-                        <input
-                            placeholder="email"
-                            type="text"
-                            name="email"
-                            value={email}
-                            onChange={this.handleChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            placeholder="username"
-                            type="text"
-                            name="username"
-                            value={username}
-                            onChange={this.handleChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            placeholder="password"
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={this.handleChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            placeholder="password_confirmation"
-                            type="password"
-                            name="password_confirmation"
-                            value={password_confirmation}
-                            onChange={this.handleChange}
-                        />
-                    </p>
-                    <input type="submit" value="Create" />
-
-                </form>
-
-            </div>
-        )
-    }
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <div style={{ color: "red" }}>{this.props.emailError}</div>
+            <input
+              placeholder="email"
+              type="text"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <div style={{ color: "red" }}>{this.props.usernameError}</div>
+            <input
+              placeholder="username"
+              type="text"
+              name="username"
+              value={username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <div style={{ color: "red" }}>{this.props.passwordError}</div>
+            <input
+              placeholder="password"
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <div style={{ color: "red" }}>{this.props.passwordConfirmationError}</div>
+            <input
+              placeholder="password_confirmation"
+              type="password"
+              name="password_confirmation"
+              value={password_confirmation}
+              onChange={this.handleChange}
+            />
+          </div>
+          <input type="submit" value="Create" />
+        </form>
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = ({ usersReducer}) => {
+  const {
+    emailError,
+    usernameError,
+    passwordError,
+    passwordConfirmationError,
+    status
+  } = usersReducer;
 
-export default connect(null, { signupUser })(Signup)
+  return {
+    emailError: emailError,
+    usernameError: usernameError,
+    passwordError: passwordError,
+    passwordConfirmationError: passwordConfirmationError,
+    status: status
+  };
+};
+
+export default connect(mapStateToProps, { signupUser })(Signup);
