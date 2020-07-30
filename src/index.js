543b77ab4920986d2a04006dd2ee1efa2fd1f2de
AssanSav <assane.savadogo81@gmail.com>
2020-07-30 15:22:52 -0400

    Error notifications added to the redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux'
-import logger from "redux-logger"
-import { rootReducer } from './reducers'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import './styleSheet/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
+import { rootReducer } from "./reducers";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import "./styleSheet/index.css";
+import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, logger)))
+let store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,5 +21,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
