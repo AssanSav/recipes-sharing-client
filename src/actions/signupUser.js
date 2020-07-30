a1a64ee64bb59a5c9847260ca9d5e27c4a39e9ff
AssanSav <assane.savadogo81@gmail.com>
2020-07-30 15:18:42 -0400

    Errors notification added in case of failed signup

--- src/actions/signupUser.js
+++ src/actions/signupUser.js
@@ -1,22 +1,30 @@
-import { SIGNUP } from "./types"
-
-
+import { SIGNUP, FAILED_SIGNUP } from "./types";
 
 export function signupUser(user) {
-    return (dispatch) => {
-        return fetch("http://localhost:3001/api/users", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify(user)
-
-        })
-            .then(resp => resp.json())
-            .then(({ user }) => {
-                dispatch({ type: SIGNUP, payload: user.data.attributes })
-            } )
-    }
+  return (dispatch) => {
+    return fetch("http://localhost:3001/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(user),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        data.status === 200
+          ? dispatch({ type: SIGNUP, payload: data.user.data.attributes })
+        :
+        dispatch(
+          {
+            type: FAILED_SIGNUP,
+            status: 409,
+            usernameError: data.username_error,
+            emailError: data.email_error,
+            passwordError: data.passwordError,
+            passwordConfirmationError: data.password_confirmation_error,
+          })
+      });
+  };
 }
